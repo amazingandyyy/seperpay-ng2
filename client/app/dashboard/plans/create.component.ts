b2a575258139ee5ed859a816c067710a96a1c647
Andy <Andy@amazingandyyy.attlocal.net>
2016-07-19 21:02:05 -0700

    working on create input form
    working on create input form

--- client/app/dashboard/plans/create.component.ts
+++ client/app/dashboard/plans/create.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'sp-create',
-    templateUrl: 'create.component.html'
+    templateUrl: 'create.component.html',
+    styleUrls: ['plans.style.css']
 })
 export class CreateComponent {
     constructor() { }
